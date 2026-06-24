8c4ebd1824902e0d5d49ac1f43ff709d5f2f5681
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-06-24 23:58:53 +0300

    v0: git init + data types

---
+++ src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // # dataTypes :
+            // * primitive
+                // 1-numeric :
+                    byte num1 = 127;
+                    short num2 = 32000;
+                    int num3 = 2000000000;
+                    long num4 = 20000000000L;
+                    float num5 = 5.5f;
+                    double num6 = 5.88;
+                // 2- non-numeric :
+                    char ch = 'c';
+                    boolean exp = true;
+            // * non-primitive
+                String sr = "jjjjh";
+                byte arr[] = {1,2,3,4,5};
+        System.out.println("Hello, World!");
+        // ----------------------------------------------------------------------------
+}
+
