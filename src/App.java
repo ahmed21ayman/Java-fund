605433f8c98e0d408bc72291c3537105db01a33e
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-06-25 00:01:49 +0300

    v1: java operators

--- src/App.java
+++ src/App.java
@@ -1,21 +1,44 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // # dataTypes :
-            // * primitive
-                // 1-numeric :
-                    byte num1 = 127;
-                    short num2 = 32000;
-                    int num3 = 2000000000;
-                    long num4 = 20000000000L;
-                    float num5 = 5.5f;
-                    double num6 = 5.88;
-                // 2- non-numeric :
-                    char ch = 'c';
-                    boolean exp = true;
-            // * non-primitive
-                String sr = "jjjjh";
-                byte arr[] = {1,2,3,4,5};
-        System.out.println("Hello, World!");
+        // // # dataTypes :
+        //     // * primitive
+        //         // 1-numeric :
+        //             byte num1 = 127;
+        //             short num2 = 32000;
+        //             int num3 = 2000000000;
+        //             long num4 = 20000000000L;
+        //             float num5 = 5.5f;
+        //             double num6 = 5.88;
+        //         // 2- non-numeric :
+        //             char ch = 'c';
+        //             boolean exp = true;
+        //     // * non-primitive
+        //         String sr = "jjjjh";
+        //         byte arr[] = {1,2,3,4,5};
+        // System.out.println("Hello, World!");
         // ----------------------------------------------------------------------------
+               // // // # operators :
+        // //         int a = 10;
+        // //         int b = 20;
+        // //         int c = 30;
+        // //         boolean x = true;
+        // //         boolean y = false;
+        // //     // * Arithmetic operators :
+        // //         int sum = a+b;
+        // //         int diff = a-b;
+        // //         int mul = a*b;
+        // //         int div = a/b;
+        // //         int mod = a%b;
+        // //     // * unary operators :
+        // //         System.out.println("Postincrement : " + (a++));
+        // //         System.out.println("Preincrement : " + (++a));
+        // //         System.out.println("Postdecrement : " + (b--));
+        // //         System.out.println("Predecrement : " + (--b));
+        // //     // * Assignment operators :
+        // //         a+= 5;
+        // //         a-= 5;
+        // //         a*= 30;
+        // //         a/= 2;
+        // //         a%= 2;
 }
 
